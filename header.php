49221823863f5fa5abbf562f387d4ebed3dc1356
Tom Madrid <madrid1979@gmail.com>
2019-01-03 11:16:26 -0800

    Remove header nav and title from page content

--- header.php
+++ header.php
@@ -39,14 +39,18 @@
 		<div class="off-canvas-wrapper">
 			
 			<!-- Load off-canvas container. Feel free to remove if not using. -->			
-			<?php get_template_part( 'parts/content', 'offcanvas' ); ?>
+			<?php //get_template_part( 'parts/content', 'offcanvas' ); ?>
 			
 			<div class="off-canvas-content" data-off-canvas-content>
-				
+        
+        <!--
 				<header class="header" role="banner">
 							
-					 <!-- This navs will be applied to the topbar, above all content 
-						  To see additional nav styles, visit the /parts directory -->
-					 <?php get_template_part( 'parts/nav', 'offcanvas-topbar' ); ?>
+          <?php
+            /* This navs will be applied to the topbar, above all content 
+               To see additional nav styles, visit the /parts directory */
+
+            //get_template_part( 'parts/nav', 'offcanvas-topbar' ); 
+          ?>
 	 	
 				</header> <!-- end .header -->
--- parts/loop-page.php
+++ parts/loop-page.php
@@ -5,19 +5,21 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
-						
+            
+  <!-- 
 	<header class="article-header">
-		<h1 class="page-title"><?php the_title(); ?></h1>
-	</header> <!-- end article header -->
+		<h1 class="page-title"><?php //the_title(); ?></h1>
+  </header>
+  -->
 					
-    <section class="entry-content" itemprop="text">
-	    <?php the_content(); ?>
+  <section class="entry-content" itemprop="text">
+	  <?php the_content(); ?>
 	</section> <!-- end article section -->
 						
 	<footer class="article-footer">
-		 <?php wp_link_pages(); ?>
+		<?php wp_link_pages(); ?>
 	</footer> <!-- end article footer -->
 						    
-	<?php comments_template(); ?>
+	<?php //comments_template(); ?>
 					
 </article> <!-- end article -->
