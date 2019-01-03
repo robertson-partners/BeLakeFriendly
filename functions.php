cb04cc695854ec8eaef4a1c9639515c6fbd0ca50
Tom Madrid <madrid1979@gmail.com>
2019-01-02 17:13:51 -0800

    WPBakery Theme integration

--- functions.php
+++ functions.php
@@ -7,6 +7,9 @@
 // Theme support options
 require_once(get_template_directory().'/functions/theme-support.php'); 
 
+// Visual Composer (WPBakery) Integration
+require_once(get_template_directory().'/functions/vc-integration.php');
+
 // WP Head and other cleanup functions
 require_once(get_template_directory().'/functions/cleanup.php'); 
 
